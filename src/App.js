12fc36c5cfac7b4b3b18df3b8e5b947e39f1402a
Abhishek Bhardwaj <abshkbh@gmail.com>
2022-03-06 18:28:31 -0800

    Add route for Login

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import Login from './Login.js';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 
 class App extends React.Component {
   render() {
-    return <Login />
+    return (
+      <div className="App">
+        <BrowserRouter>
+          <Routes>
+            <Route exact path="/" element={<Login />} />
+          </Routes>
+        </BrowserRouter>
+      </div >
+    );
   }
 }
 
